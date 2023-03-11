db3f4583394cc38f4ab26bffa28cbff829d2ec8f
hasnainhawlader <hasnain.hawlader@gmail.com>
2023-03-12 00:23:52 +0600

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,17 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
-const App = () => {
-  const { user } = useAuthContext();
 
+const App = () => {
   return (
     <div className="app container mx-auto">
-      <div className="upperlevel-contents min-h-screen">
-        <Routes>
-          <Route path="/" element={<Chat />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </div>
   );
 };
+export default App;
