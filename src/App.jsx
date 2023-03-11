0b253038b5052610d4600fdb76073606c82358f2
hasnainhawlader <hasnain.hawlader@gmail.com>
2023-03-12 00:20:06 +0600

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-function App() {
-  return <div className="text-8xl">hi</div>;
-}
+import { Routes, Route, Navigate } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
+const App = () => {
+  const { user } = useAuthContext();
 
-export default App;
+  return (
+    <div className="app container mx-auto">
+      <div className="upperlevel-contents min-h-screen">
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
