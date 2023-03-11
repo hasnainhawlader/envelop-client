a2233d462000a430325b20ce76a4959e9fb84d49
hasnainhawlader <hasnain.hawlader@gmail.com>
2023-03-12 00:34:30 +0600

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -6,12 +8,16 @@ import Register from "./pages/Register";
 const App = () => {
   return (
     <div className="app container mx-auto">
-      <Routes>
-        <Route path="/" element={<Chat />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <Navbar />
+      <div className="upperlevel-container min-h-screen">
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-center py-20 text-gray-500">
+      <p>&copy; {new Date().getFullYear()} Envelop all right reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
