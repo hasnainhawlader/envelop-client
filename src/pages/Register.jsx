6d7c158d761ef59569b63ae767cee4a088fbfad1
hasnainhawlader <hasnain.hawlader@gmail.com>
2023-03-13 00:17:37 +0600

    section title added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import SectionTitle from "../components/SectionTitle";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [formFields, setFormFields] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  return (
+    <div className="register flex flex-col justify-center  items-center">
+      <SectionTitle title={"Register...."} />
+    </div>
+  );
 };
 
 export default Register;
