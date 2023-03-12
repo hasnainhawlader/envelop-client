b6013213e0137a3ebd7707af39ef39af9ee03171
hasnainhawlader <hasnain.hawlader@gmail.com>
2023-03-13 00:05:09 +0600

    nav added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="py-10 flex  justify-between">
+      <div className="logo text-xl ">
+        <Link to="/" className="font-semibold hover:text-violet-500">
+          Envelop
+        </Link>
+      </div>
+      <nav>
+        <div className="flex gap-5">
+          <Link to="/login" className=" hover:text-violet-500">
+            Login
+          </Link>
+          <Link to="/register" className=" hover:text-violet-500">
+            Register
+          </Link>
+        </div>
+        {/*<div>
+          <p>
+            Login as : <span>HK</span>
+          </p>
+  </div>*/}
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
